e809f9afeb662b252405a8bd321d18a2e5967bee
Emil de Valk <e.devalk@stamkracht.com>
2011-08-11 18:09:21 +0200

    Added some documentation on the use of the calendar UX.

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -16,11 +16,18 @@ var bruens = {
   
   /**
    * method to generate jquery Ui calendars.
+   * This function expects a structure like:
+   * <div>
+   *  <div class="boxCalendar"></div>
+   *  <div class="fldDate">
+   *    <input type="text" name="x" id="y" />
+   *  </div>
+   * </div>
    */
   getCalendars: function() {
     $('.boxCalendar').datepicker({
-      dateFormat: 'dd-mm-yy',
-      onSelect:   function(dateText, inst) {
+      dateFormat: 'dd-mm-yy', // format 23-11-2011
+      onSelect:   function(dateText, inst) { //when a date is selected, populate the date-field
         $(this).parent().find('.fldDate input').val(dateText);
       }
     });
@@ -28,7 +35,8 @@ var bruens = {
   
 }
 
-
+// When the dom is ready, 
+// fire the bruens initializer!
 $(document).ready(function(){
   bruens.init();
 });
